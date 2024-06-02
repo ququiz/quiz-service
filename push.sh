78fb6d9f71ec064239877256b80ac916e76aa0fe
lintang-b-s <lintangbirdasaputra23@gmail.com>
2024-06-02 17:53:34 +0700

    add jenkinsfile ci build & push image

--- push.sh
+++ push.sh
@@ -1,3 +1,3 @@
-docker build . -t lintangbirdas/quiz-command-service:v1
-docker tag lintangbirdas/quiz-command-service:v1 lintangbirdas/quiz-command-service:v1
-docker push lintangbirdas/quiz-command-service:v1
+docker build . -t lintangbirdas/quiz-command-service-adam:v1
+docker tag lintangbirdas/quiz-command-service-adam:v1 lintangbirdas/quiz-command-service-adam:v1
+docker push lintangbirdas/quiz-command-service-adam:v1
