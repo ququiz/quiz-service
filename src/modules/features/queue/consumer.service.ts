ffd628351a1b9b53dd81a8d3027604cbd37dae2a
Alexander Adam <alexanderadamm21@gmail.com>
2024-06-03 17:28:28 +0700

    feat: implement quiz user answer message queue consumer

--- src/modules/features/queue/consumer.service.ts
+++ src/modules/features/queue/consumer.service.ts
@@ -1,27 +1,44 @@
 import { Injectable, OnModuleInit, Logger } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
 import amqp, { ChannelWrapper } from 'amqp-connection-manager';
 import { ConfirmChannel } from 'amqplib';
+import { UserAnswerMQDTO } from './dtos/user-answer.dto';
 
 @Injectable()
 export class ConsumerService implements OnModuleInit {
   private channelWrapper: ChannelWrapper;
   private readonly logger = new Logger(ConsumerService.name);
-  constructor(private readonly configService: ConfigService) {
-    const connection = amqp.connect([
-      this.configService.get<string>('RABBITMQ_URL'),
-    ]);
-    this.channelWrapper = connection.createChannel();
+  private readonly EXCHANGE_NAME = 'quiz-command-quiz-query';
+  private readonly EXCHANGE_TYPE = 'topic';
+  private readonly QUEUE_NAME = 'userAnswerQueue';
+  private readonly ROUTING_KEY = 'user-answer';
+
+  constructor() {
+    const connection = amqp.connect(['amqp://localhost']);
+    this.channelWrapper = connection.createChannel({
+      setup: async (channel: ConfirmChannel) => {
+        await channel.assertExchange(this.EXCHANGE_NAME, this.EXCHANGE_TYPE, {
+          durable: true,
+        });
+        await channel.assertQueue(this.QUEUE_NAME, { durable: true });
+        await channel.bindQueue(
+          this.QUEUE_NAME,
+          this.EXCHANGE_NAME,
+          this.ROUTING_KEY,
+        );
+      },
+    });
   }
 
   public async onModuleInit() {
     try {
       await this.channelWrapper.addSetup(async (channel: ConfirmChannel) => {
-        await channel.assertQueue('quiz-queue', { durable: true });
-        await channel.consume('quiz-queue', async (message) => {
+        await channel.consume(this.QUEUE_NAME, async (message) => {
           if (message) {
-            const content = JSON.parse(message.content.toString());
+            const content = JSON.parse(
+              message.content.toString(),
+            ) as UserAnswerMQDTO;
             this.logger.log('Received message:', content);
+            // TODO: input the logic to save the user answer to the database
             channel.ack(message);
           }
         });
---
+++ src/modules/features/queue/dtos/user-answer.dto.ts
@@ -0,0 +1,7 @@
+export class UserAnswerMQDTO {
+  quiz_id: string;
+  question_id: string;
+  choice_id: string;
+  participant_id: string;
+  answer: string;
+}
