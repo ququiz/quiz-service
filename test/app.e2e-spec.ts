c6006fb84307703986f9100a6ac4060f57d71b9c
Andreas Notokusumo <andreasnotokusumo23@gmail.com>
2024-06-04 17:00:25 +0700

    :sparkles: E2E Test

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,5 +1,5 @@
 import { Test } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
 import * as request from 'supertest';
 import {
   MongoDBContainer,
@@ -10,6 +10,14 @@ import { QuizController } from 'src/modules/features/quiz/quiz.controller';
 import { QuizService } from 'src/modules/features/quiz/quiz.service';
 import { BaseQuiz } from 'src/modules/datasources/entities/base-quiz.entity';
 import { BaseQuizRepository } from 'src/modules/datasources/repositories/base-quiz.repository';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { CronModule } from 'src/modules/commons/cron/cron.module';
+import { AuthModule } from 'src/modules/commons/auth/auth.module';
+import { UsersModule } from 'src/modules/commons/users/users.module';
+import { InternalService } from 'src/modules/features/internal/internal.service';
+import { ProducerService } from 'src/modules/commons/queue/producer.service';
+import { ConsumerService } from 'src/modules/commons/queue/consumer.service';
+import { JwtGuard } from 'src/modules/commons/auth/guards/jwt.guard';
 
 describe('QuizController (e2e)', () => {
   let app: INestApplication;
@@ -23,22 +31,43 @@ describe('QuizController (e2e)', () => {
 
     const appModule = await Test.createTestingModule({
       imports: [
+        ConfigModule.forRoot(),
         TypeOrmModule.forRoot({
           directConnection: true,
           type: 'mongodb',
           url,
-          database: 'order-service',
           entities: [BaseQuiz],
         }),
+        TypeOrmModule.forFeature([BaseQuiz]),
+        AuthModule,
+        CronModule,
+        UsersModule,
       ],
       controllers: [QuizController],
-      providers: [QuizService, BaseQuizRepository],
-    }).compile();
+      providers: [
+        QuizService,
+        ConfigService,
+        BaseQuizRepository,
+        InternalService,
+        ConfigService,
+        ProducerService,
+        ConsumerService,
+      ],
+    })
+      .overrideGuard(JwtGuard)
+      .useValue({ canActivate: () => true })
+      .compile();
 
     quizRepository = appModule.get<BaseQuizRepository>(BaseQuizRepository);
 
     app = appModule.createNestApplication();
-
+    app.useGlobalPipes(
+      new ValidationPipe({
+        transform: true,
+        transformOptions: { enableImplicitConversion: true },
+        validateCustomDecorators: true,
+      }),
+    );
     await app.init();
   }, 70000);
 
@@ -47,23 +76,27 @@ describe('QuizController (e2e)', () => {
     await mongodbContainer.stop();
   });
 
-  it('/quiz 9 (POST)', async () => {
+  it('/quiz (POST)', async () => {
     const payload = {
       title: 'Quiz Test #2 (Cron)',
       questions: [
         {
           question: 'Test?',
-          type: 'Essay',
+          type: 'ESSAY',
           essay_answer: 'Hell no!',
           weight: 2,
         },
       ],
-      start_time: new Date(Date.now()),
-      end_time: new Date(Date.now() + 1000 * 60 * 60 * 24),
+      start_time: 1816465358423,
+      end_time: 1816468358423,
     };
 
     const response = await request(app.getHttpServer())
       .post('/quiz')
+      .set(
+        'Authorization',
+        'Bearer eyJhbGciOiJFUzUxMiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImFuZHJlbmsyIiwiaWF0IjoxNzE3NDk0MDA1LCJleHAiOjE3MTc0OTQ5MDUsInN1YiI6Ijg5YjhmNTQyLTBkMDUtNGEzZi1hOWI1LTI0MzFhNWFjZWQwYSJ9.AcaA2RbpL3sE2Y3-e7twc18FwEMPeO4OXZarVX8frBmgKkKVe0vVxx4I2kGBGzKMoyiSL7v40RrmhO_tUXLWg0GHAajv4hroO-VmaOdjfXrmo5mcl1sIeUb6HdW4Li4ZLj4j5DKi2YlM7Djwl5heWlNhL_7SizbiuWtW8Z9pS8Vnrntf',
+      )
       .send(payload)
       .expect(201);
 
@@ -71,6 +104,8 @@ describe('QuizController (e2e)', () => {
       response.body.data.created_quiz.id,
     );
 
-    expect(response.body.data.created_quiz.id).toEqual({ quizId: quiz._id });
+    expect(response.body.data.created_quiz.id).toStrictEqual(
+      quiz._id.toHexString(),
+    );
   });
 });
